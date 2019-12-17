326292ea2b9541d49dfc11ff70b217a082960a9f
Aimi <aimi@smarte.com.au>
2019-12-17 14:17:18 +1000

    update based on illuminate 6 worker construction

--- src/HorizonServiceProvider.php
+++ src/HorizonServiceProvider.php
@@ -163,7 +163,7 @@ class HorizonServiceProvider extends ServiceProvider
     protected function registerQueueWorkCommand()
     {
         $this->app->singleton(WorkCommand::class, function ($app) {
-            return new WorkCommand($app['queue.worker']);
+            return new WorkCommand($app['queue.worker'], $this->app['redis']);
         });
     }
 
